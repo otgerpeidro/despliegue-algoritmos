074136558ea48a609b109c609868bf9cf5e50cea
Otger Peidro <otgerpeidro@gmail.com>
2024-05-18 18:58:32 +0200

    Update iniciar_mlflow_ngrok.py

--- Parte1MLflow/iniciar_mlflow_ngrok.py
+++ Parte1MLflow/iniciar_mlflow_ngrok.py
@@ -10,6 +10,6 @@ mlflow_ui_process = subprocess.Popen(['mlflow', 'ui', '--port', '5000'])
 print("MLflow server is running on http://localhost:5000")
 
 # Inicia ngrok
-ngrok.set_auth_token('2gdXrCX0L9zRaeb1pPGAKqOC8xT_5g4MitQHBXE4g8934gYgg')  # Token de autenticación de ngrok
+ngrok.set_auth_token('XXXXXX')  # Token de autenticación de ngrok
 ngrok_tunnel = ngrok.connect(addr='5000', proto='http', bind_tls=True)
 print(f'El tracking UI de MLflow está disponible en: {ngrok_tunnel.public_url}')
